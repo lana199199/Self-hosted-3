8888e4ab49a3b998be4d0c222cff88732dd46ec3
thanh.pnguyen <thanh.pnguyen@192.168.117.104>
2022-09-17 17:54:50 +0700

    Hosted3

---
+++ Object Repository/Page_snsd - Tm trn Google/div_DJ min Ty Hyoyeon (SNSD) v Vit Nam biu _c36c4d.rs
@@ -0,0 +1,126 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>div_DJ min Ty Hyoyeon (SNSD) v Vit Nam biu _c36c4d</name>
+   <tag></tag>
+   <elementGuidId>7408c2b5-3487-43f8-9008-fe4734307311</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@id='rso']/div/div/g-section-with-header/div[3]/a/div/div[2]/div[2]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>div</value>
+      <webElementGuid>a184d8eb-45c1-450c-bccb-c383901783c2</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>mCBkyc tNxQIb ynAwRc nDgy9d</value>
+      <webElementGuid>ec238795-e84d-45e0-96f9-06f868ddc228</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>aria-level</name>
+      <type>Main</type>
+      <value>4</value>
+      <webElementGuid>734adc35-2db2-4fe4-99b9-242dfebb79c0</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>role</name>
+      <type>Main</type>
+      <value>heading</value>
+      <webElementGuid>1f89d1e8-acac-491b-a06f-4ad19ee2b0bf</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>“DJ miền Tây” Hyoyeon (SNSD) về Việt Nam biểu diễn, fan túc trực đến tận 2 
+giờ sáng</value>
+      <webElementGuid>5d10f447-b6d7-449d-a672-f5d4b3077c72</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;rso&quot;)/div[@class=&quot;ULSxyf&quot;]/div[@class=&quot;MjjYud&quot;]/g-section-with-header[@class=&quot;yG4QQe TBC9ub&quot;]/div[3]/a[@class=&quot;WlydOe&quot;]/div[@class=&quot;vJOb1e aIfcHf&quot;]/div[@class=&quot;iRPxbe&quot;]/div[@class=&quot;mCBkyc tNxQIb ynAwRc nDgy9d&quot;]</value>
+      <webElementGuid>baa45ed7-0113-46ab-b7c1-c97d1872d4d2</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='rso']/div/div/g-section-with-header/div[3]/a/div/div[2]/div[2]</value>
+      <webElementGuid>79c81a53-c9bb-4b7e-a1ad-db5934a35969</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Kenh14'])[1]/following::div[1]</value>
+      <webElementGuid>6922a0f8-6139-47f9-808c-777477cb158e</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Hyoyeon (SNSD), Hoylin khoe ảnh ở Việt Nam'])[1]/following::div[9]</value>
+      <webElementGuid>0cbd88ee-4ca3-44b7-aaf4-2e1192b4156c</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Xem tin khác'])[1]/preceding::div[2]</value>
+      <webElementGuid>57e7b60c-2180-4fdd-b747-dd72a286e7a7</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)=concat('Girls', &quot;'&quot;, ' Generation – Wikipedia tiếng Việt')])[1]/preceding::div[3]</value>
+      <webElementGuid>e602e741-b935-4b31-85b2-8d2da8a70a90</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div[3]/a/div/div[2]/div[2]</value>
+      <webElementGuid>6ed03144-ac55-459d-b4e1-63f248682c62</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//div[(text() = '“DJ miền Tây” Hyoyeon (SNSD) về Việt Nam biểu diễn, fan túc trực đến tận 2 
+giờ sáng' or . = '“DJ miền Tây” Hyoyeon (SNSD) về Việt Nam biểu diễn, fan túc trực đến tận 2 
+giờ sáng')]</value>
+      <webElementGuid>7bd426c4-c220-4465-869d-7e41e2bcd756</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
---
+++ Test Suites/TS_hosted3.ts
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS_hosted3</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>eb628f49-b3db-41c0-a46c-c2e9bfd77b17</testSuiteGuid>
+   <testCaseLink>
+      <guid>e0749d6b-880d-473f-ac75-1ebde8fb0876</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC_hosted3</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>4b872388-09d5-40ca-a4b4-e5e9c40c8841</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC_hosted3 - Copy</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
